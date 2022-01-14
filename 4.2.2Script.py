7854a28ad1f1e5e09b0460f33a2fb1ad5233158c
keqpup232 <keqpup232@gmail.com>
2022-01-14 12:14:51 +0300

    Homework 4.2 fix

--- 4.2.2Script.py
+++ 4.2.2Script.py
@@ -11,5 +11,5 @@ bash_command = ["cd " + arg1, "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
 for result in result_os.split('\n'):
     if result.find('modified') != -1:
-        prepare_result = result.replace('\tmodified:   ', '')
+        prepare_result = result.replace('\tmodified:   ', 'modified:   '+arg1+'/')
         print(prepare_result)
