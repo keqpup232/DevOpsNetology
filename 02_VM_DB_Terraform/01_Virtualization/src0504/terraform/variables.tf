56ba58640d3c4b78d84bbc372b64bd66526d486f
Ivan Denisovich <keqpup23@gmail.com>
2022-01-30 22:44:36 +0300

    5.4 HomeWork release

---
+++ 02_VM_DB_Terraform/01_Virtualization/src0504/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1ghqnp2h5jhkaihdie3"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gtuo5tk747fe1j8fc9"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd84tn1of8p28fn0shoj"
+}
