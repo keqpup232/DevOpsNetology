9f2c66ffa01140d6c3151ef50d648abb9ee6630b
Ivan Gavryushin <keqpup232@gmail.com>
2022-04-09 23:26:44 +0300

    7.4 Homework done

---
+++ 02_VM_DB_Terraform/03_Terraform/7.4/TerraformModule/main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  subnet_id              = aws_subnet.my_subnet.id
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
